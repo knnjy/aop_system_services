313c79d18ea839102cda710c046e2d369c914ff7
MicoGenosas <2024-200074@rtu.edu.ph>
2026-05-10 20:50:53 +0800

    Remove deprecated book_catalog_controller

--- app/controllers/book_catalog_controller.py
+++
@@ -1,7 +0,0 @@
-import fastapi
-
-# This controller is now deprecated - all book operations are handled by book_controller.py
-# This file is kept for backward compatibility only
-
-router = fastapi.APIRouter(prefix="/api/books")
-
