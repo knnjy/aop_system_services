2d99d25fb262bf05e428c79ce14317399d0fe934
Mico Genosas <2024-200074@rtu.edu.ph>
2026-05-15 01:42:30 +0800

    Implement OrderDAO with CSV loading and order methods

--- app/dao/orders_dao.py
+++ app/dao/orders_dao.py
@@ -1,8 +1,23 @@
-
-
 from app.utils.csv_loader import load_csv
-
 
 class OrderDAO:
     def __init__(self) -> None:
-        self._orders = load_csv("")
+        self._orders = load_csv("orders.csv")
+
+    def get_order_by_id(self, id: int):
+        for order in self._orders:
+            if order.id == id:
+                return order
+        return None
+
+    def save_order(self, order):
+        for idx, existing_order in enumerate(self._orders):
+            if existing_order.id == order.id:
+                self._orders[idx] = order
+                break
+        self._write_to_csv()
+
+    def _write_to_csv(self):
+
+        pass
+
