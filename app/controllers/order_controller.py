fd84307a075f74d963faaeb91eeb9d451961b59e
Mico Genosas <2024-200074@rtu.edu.ph>
2026-05-15 01:10:03 +0800

    Add update_order endpoint to order_controller

--- app/controllers/order_controller.py
+++ app/controllers/order_controller.py
@@ -1,4 +1,5 @@
 from app.dto.order_dto import OrderRequest
+from app.dto.order_dto import OrderUpdate
 from app.services.order_service import OrderService
 from fastapi import APIRouter
 
@@ -9,6 +10,11 @@ _order_service = OrderService()
 @router.get("/")
 def home():
     return {"message": "Order Management API Working"}
+
+@router.put("/update-order/{id}")
+def update_order(id: int, order_update: OrderUpdate):
+    return _order_service.update_order(id, order_update)
+
 
 @router.post("/add-order")
 def create_order(order_form: OrderRequest):
